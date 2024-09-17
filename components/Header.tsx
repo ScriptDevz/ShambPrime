2073f37c485a9c67d6828229111c8599e3ea4303
Michael <mastringer87@gmail.com>
2024-09-16 20:04:12 -0700

    update push

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,14 @@
 "use client";
 
 export default function Header() {
-    return <header>Header Menu Bar Section</header>;
+    return <header>
+        <input/>
+        Free Movies & TV
+        Live TV
+        Features
+        Download
+        |
+        Open ShambPrime
+        Profile
+    </header>;
 }
