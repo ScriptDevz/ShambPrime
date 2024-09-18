9e7cf134b9605073c65b533fad123337393f3203
Paul De Silos <paul_desilos@hotmail.com>
2024-09-17 23:28:46 -0500

    "FAQ" section formatted

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import "../css/faq.css"
+
 export default function FAQ() {
-    return <div>FAQ Section</div>;
+    return <section id="faq-list">
+        <div className="inside-faq">
+            <div className="headline-faq">
+                <h2>Question? ... Answer!</h2>
+            </div>
+            <div className="down-faq-list">
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+                <div className="down-faq-list-inner">Lorem Ipsum Bullcrap Text Here</div>
+            </div>
+        </div>
+    </section>
 }
